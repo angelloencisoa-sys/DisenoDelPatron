2ebc03f220bf67aa34255e350d80be02bcc7d7ee
Yahir <yhuairayauri@gmail.com>
2026-06-13 17:30:02 -0500

    Los repository Final

--- src/main/java/pe/edu/utp/PF/repository/AsesorFinancieroRepository.java
+++ src/main/java/pe/edu/utp/PF/repository/AsesorFinancieroRepository.java
@@ -3,7 +3,9 @@ package pe.edu.utp.PF.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pe.edu.utp.PF.model.AsesorFinanciero;
 
+import java.util.List;
+
 public interface AsesorFinancieroRepository extends JpaRepository<AsesorFinanciero, Long> {
 
-    java.util.List<AsesorFinanciero> findByCodigoAgencia(String codigoAgencia);
+    List<AsesorFinanciero> findByCodigoAgencia(String codigoAgencia);
 }
