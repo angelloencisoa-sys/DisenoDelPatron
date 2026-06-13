4ad0a3f338fd6e3fc84a006f620162724f70a3b6
Angello <angelloencisoa@gmail.com>
2026-06-13 17:22:00 -0500

    cambios xd

--- src/main/java/pe/edu/utp/PF/model/SolicitudCredito.java
+++ src/main/java/pe/edu/utp/PF/model/SolicitudCredito.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pe.edu.utp.PF.service.patron.prototype.Contrato;
 
 @Entity
 @AllArgsConstructor
